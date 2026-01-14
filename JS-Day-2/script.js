beb70d18a0dc0b81d59579568b6da908b48e8a38
Piyush <piyushjain1857@gmail.com>
2026-01-14 09:52:38 +0530

    questions to do

--- JS-Day-2/script.js
+++ JS-Day-2/script.js
@@ -2,12 +2,12 @@
 let arr = [1, 2, 3, 4, 5, 6]
 for (i = 0; i < arr.length; i++) {
     arr[i] = arr[i] + 3;
-} 
+}
 console.log(arr);
 
 // ForEach Example: Add 3 to each element in the array
 let arr2 = [1, 2, 3, 4, 5, 6]
-arr2.forEach(function(element, index, array) {
+arr2.forEach(function (element, index, array) {
     array[index] = element + 3;
 });
 console.log(arr2);
@@ -15,7 +15,14 @@ console.log(arr2);
 
 // Map Example: Create a new array with each element increased by 3
 let arr3 = [1, 2, 3, 4, 5, 6]
-let newArr = arr3.map(function(i) {
+let newArr = arr3.map(function (i) {
     return i + 3;
 });
 console.log(newArr);
+
+for (let i = 1; i <= 5; i++) {
+    for (let j = 1; j <= i; j++) {
+        console.log(j);
+    }
+    console.log(i);
+}
