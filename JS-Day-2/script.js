3632956d939107b016f337515317fee4fbd4313c
Piyush <piyushjain1857@gmail.com>
2026-01-14 09:44:12 +0530

    Foreach Method

--- JS-Day-2/script.js
+++ JS-Day-2/script.js
@@ -4,3 +4,11 @@ for (i = 0; i < arr.length; i++) {
     arr[i] = arr[i] + 3;
 } 
 console.log(arr);
+
+// ForEach Example: Add 3 to each element in the array
+let arr2 = [1, 2, 3, 4, 5, 6]
+arr2.forEach(function(element, index, array) {
+    array[index] = element + 3;
+});
+console.log(arr2);
+
