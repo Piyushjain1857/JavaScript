b4940bdd91eddbd8b27c54f562ae27b2e9a05b21
Piyush <piyushjain1857@gmail.com>
2026-01-17 22:33:23 +0530

    made the car move

--- JS_For_Practice/car/script.js
+++ JS_For_Practice/car/script.js
@@ -1,3 +1,17 @@
 const car = document.getElementById("car");
 
+let position = 0;
+let moveInterval;
 
+car.addEventListener("mouseenter", () => {
+    moveInterval = setInterval(() => {
+        if (position < window.innerWidth - 100) {
+            position += 5;
+            car.style.left = position + "px";
+        }
+    }, 20);
+});
+
+car.addEventListener("mouseleave", () => {
+    clearInterval(moveInterval);
+});
