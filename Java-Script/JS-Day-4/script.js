d76a78f73a5f5a3cef59461335ce778704b97b8c
Piyush <piyushjain1857@gmail.com>
2026-01-20 21:14:04 +0530

    added string codes

--- Java-Script/JS-Day-4/script.js
+++ Java-Script/JS-Day-4/script.js
@@ -20,3 +20,62 @@
 
 
 //  do string methods
+
+// slice
+
+// let str = "JavaScript is fun!";
+// let sliced = str.slice(0, 10);
+
+// substring
+
+// let str = "JavaScript is amazing!";
+// let sub = str.substring(11, 17);
+
+// indexOf()
+
+// const paragraph = 'The quick brown fox jumps over the lazy dog. If the dog barked, was it really lazy?';
+// const indexOfFirst = paragraph.indexOf("dog");
+// console.log(indexOfFirst);
+
+// substr
+
+// let str = "OpenAI is innovative!";
+// let sub = str.substr(8, 2);
+
+// replace
+
+// let sentence = "I love JavaScript. JavaScript is powerful.";
+// let newSentence = sentence.replace("JavaScript", "Node.js");
+
+// replaceAll
+
+// let sentence = "I love JavaScript. JavaScript is powerful.";
+// let newSentence = sentence.replaceAll("JavaScript", "Node.js");
+
+// toUpperCase() and toLowerCase()
+
+// let name = "John Doe";
+// let upperCaseName = name.toUpperCase(); 
+// let lowerCaseName = name.toLowerCase(); 
+
+// concat
+
+// let firstName = "John";
+// let lastName = "Doe";
+// let fullName = firstName.concat(" ", lastName);
+
+// trim()
+
+// let sentence = "   This is a sentence with spaces.   ";
+// let trimmed = sentence.trim();
+
+// charAt(index) and charCodeAt(index)
+
+// let word = "Hello";
+// let char = word.charAt(1); 
+// let charCode = word.charCodeAt(1);
+
+// split
+
+// let sentence = "I,am,a,comma,separated,sentence";
+// let words = sentence.split(",");
