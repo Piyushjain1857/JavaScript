cd92e21d2f22682e5e5674bc96573dec74288414
Piyush <piyushjain1857@gmail.com>
2026-01-28 09:33:20 +0530

    created user

--- Java-Script/JS-Day-7/script.js
+++ Java-Script/JS-Day-7/script.js
@@ -1,5 +1,15 @@
-[
-    ["x", "o", "x"],
-    ["x", "o", "x"],
-    ["x", "o", "x"]
-]
+// [
+//     ["x", "o", "x"],
+//     ["x", "o", "x"],
+//     ["x", "o", "x"]
+// ]
+
+let user={
+    name:"Piyush",
+    age:21,
+    skills:["HTML","CSS","JS"],
+    addres:"Gurugram",
+    favcolor:["red","blue","green"]
+}
+
+console.log(user);
